ccaa3f0f9ce1606b5d0c919b993ec5e79c4f7f43
rafael.asencio <rafael.asencio@abancagen.com>
2024-04-19 11:05:38 +0200

    Initial Commit

---
+++ rickymorty/rickymortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickymortyApp.swift
+//  rickymorty
+//
+//  Created by Rafael Asencio Blancat on 19/4/24.
+//
+
+import SwiftUI
+
+@main
+struct rickymortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
